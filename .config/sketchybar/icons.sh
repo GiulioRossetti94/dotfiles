1e30481b398884f4a7ff5329bd912cb375bdc640
Giulio Rossetti <giulio.rossetti94@gmail.com>
2023-08-08 19:25:40 +0100

    yabai and skhd + sketchybar

---
+++ .config/sketchybar/icons.sh
@@ -0,0 +1,6 @@
+#                          ﱦ 齃     ﮂ  ﵁ 爵        ﭵ     ﱦ  ﰊ 異 ﴱ אַ  'Y⃣'
+SPOTIFY_BACK=􀊎
+SPOTIFY_PLAY_PAUSE=􀊈
+SPOTIFY_NEXT=􀊐
+SPOTIFY_SHUFFLE=􀊝
+SPOTIFY_REGPEAT=􀊞
